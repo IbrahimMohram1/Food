63d7008d9922e3f9a089b2f21daba39ef93595be
IbrahimMohram1 <ibrahimmohram7@gmail.com>
2024-10-24 00:48:38 +0300

    Update

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -53,7 +53,6 @@ export default function Navbar() {
             <ul className="font-medium flex flex-col p-4 md:p-0 mt-4  md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0">
               <li>
                 <Link
-                  onClick={() => MealsByArea()}
                   to={"/"}
                   className="block py-2 px-3 text-dark"
                   aria-current="page"
